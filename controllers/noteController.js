5f24a700f0791a7a538b14bb90f896b3b38a4cf0
Raahim Fareed <raahimfareed@proton.me>
2024-08-28 04:54:19 +0500

    changed short names to full names and added update and delete note functions

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -2,17 +2,17 @@ import Note from "../models/Note.js";
 import mongoose from "mongoose";
 
 // get all notes
-export const getNotes = async (req, res) => {
+export const getNotes = async (request, response) => {
   const notes = await Note.find({}).sort({ createdAt: -1 });
 
-  res.status(200).json(notes)
+  response.status(200).json(notes)
 }
 
 // get a single note
-export const getNote = async (req, res) => {
-  const { id } = req.params;
+export const getNote = async (request, response) => {
+  const { id } = request.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({
+    return response.status(404).json({
       error: "Note not found"
     })
   }
@@ -20,17 +20,17 @@ export const getNote = async (req, res) => {
   const note = await Note.findById(id);
 
   if (!note) {
-    return res.status(404).json({
+    return response.status(404).json({
       error: "Note not found"
     })
   }
 
-  res.status(200).json(note);
+  response.status(200).json(note);
 }
 
 // create a new note
-export const createNote = async (req, res) => {
-  const { title, body } = req.body;
+export const createNote = async (request, response) => {
+  const { title, body } = request.body;
 
   // Add document to database
   try {
@@ -38,28 +38,56 @@ export const createNote = async (req, res) => {
       title,
       body
     });
-    res
+    response
       .status(200)
       .json(note);
   } catch (error) {
     console.error("Unable to make note")
     console.error(error);
-    res
+    response
       .status(400)
       .json({ message: error.message })
   }
 }
 
 // delete a note
-export const deleteNote = async (req, res) => {
-  res.json({
-    message: "Deleting a note"
-  });
+export const deleteNote = async (request, response) => {
+  const { id } = request.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(404).json({
+      error: "Note not found"
+    })
+  }
+
+  const note = await Note.findOneAndDelete({ _id: id });
+
+  if (!note) {
+    return response.status(400).json({
+      error: "Note not found"
+    })
+  }
+
+  response.status(200).json(note);
 }
 
 // update a note
-export const updateNote = async (req, res) => {
-  res.json({
-    message: "Updating a note"
+export const updateNote = async (request, response) => {
+  const { id } = request.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(404).json({
+      error: "Note not found"
+    })
+  }
+  
+  const note = await Note.findOneAndUpdate({ _id: id }, {
+    ...request.body
   });
+
+  if (!note) {
+    return response.status(400).json({
+      error: "Note not found"
+    })
+  }
+
+  response.status(200).json(note);
 }
