40a20e9e25dcf0fa8355bd501c65fa2f81e99574
Raahim Fareed <raahimfareed@proton.me>
2024-08-29 20:40:50 +0500

    added cors and helmet middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import helmet from "helmet";
+import cors from "cors";
 import "dotenv/config"
 import "dotenv-expand/config"
 
@@ -10,6 +12,9 @@ const app = express();
 
 // Middleware
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -17,7 +22,6 @@ app.use((req, res, next) => {
 
 app.use('/api', authRouter);
 app.use('/api/notes', notesRouter);
-
 
 // Connect to database
 mongoose
